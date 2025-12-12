1db06c77f15a2f8fba2155e24c35ed9f47348547
Мария Суханова <mariasukh2006@gmail.com>
2025-12-12 16:30:54 +0300

    added base for ench

---
+++ src/InventorySystem.Core/Items/Abstractions/EnchantmentBase.cs
@@ -0,0 +1,34 @@
+using InventorySystem.Core.Items.Interfaces;
+
+namespace InventorySystem.Core.Items.Enchantment
+{
+    public abstract class BaseEnchantment : IEnchantment
+    {
+        public string Name { get; protected set; }
+        public int Level { get; protected set; }
+        public EnchantmentType Type { get; protected set; }
+        
+        protected Dictionary<string, float> _bonuses = new Dictionary<string, float>();
+        
+        protected BaseEnchantment(string name, int level, EnchantmentType type)
+        {
+            Name = name;
+            Level = level;
+            Type = type;
+            InitializeBonuses();
+        }
+        
+        protected abstract void InitializeBonuses();
+        
+        public virtual float GetBonus(string statType)
+        {
+            if (_bonuses.ContainsKey(statType))
+                return _bonuses[statType];
+            return 0f;
+        }
+        
+        public virtual void ApplyEffect(ICharacter target)
+        {
+        }
+    }
+}
