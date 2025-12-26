58a5dd8f01e35ffed5c3bb9b9933072a20ac4906
Мария Суханова <mariasukh2006@gmail.com>
2025-12-26 15:18:46 +0300

    slight changes

--- src/InventorySystem.Core/States/Interfaces/IStatefulItem.cs
+++ src/InventorySystem.Core/States/Interfaces/IStatefulItem.cs
@@ -4,8 +4,6 @@ namespace InventorySystem.Core.Items.Interfaces
 {
     public interface IStatefulItem : IItem
     {
-        IItemState CurrentState { get; }
         void SetState(IItemState newState);
-        float GetCurrentEffectiveness();
     }
 }
